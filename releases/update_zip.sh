4452ff40e5223f3bc2a0c4408f10008e2b29de85
Dennis Pater <d.pater1986@gmail.com>
2020-09-06 13:06:39 +0200

    The way of updating after edit

---
+++ releases/update_zip.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# get version number
+version=($(grep -oP '(?<=version>)[^<]+' "templateDetails.xml"))
+echo What is the version number now: ${version[0]}
+echo What is the version number you want it to get:
+read version2
+echo $version2
+
+# start to change version number
+sed -i 's/'$version'/'$version2'/' "templateDetails.xml"
+sed -i '$d' "releases/update.xml"
+sed -i 's/'$version'/'$version2'/' "releases/.newUpdate.xml"
+cat "releases/.newUpdate.xml" >> "releases/update.xml"
+echo "</updates>" >> "releases/update.xml"
+
+zip "releases/msjt_v"$version2".zip" css/* images/* index.php templateDetails.xml
+cp "releases/msjt_v"$version2".zip" "releases/msjt_latest.zip"
