ead1d33868643398e24dd241368391323839176b
Dennis Pater <d.pater1986@gmail.com>
2020-09-14 18:11:42 +0200

    Forgot to commit

--- releases/update_zip.sh
+++ releases/update_zip.sh
@@ -7,19 +7,36 @@ version=($(grep -oP '(?<=version>)[^<]+' "templateDetails.xml"))
 echo What is the version number now: ${version[0]}
 echo What is the version number you want it to get:
 read version2
+echo add a tag?
+echo yes rest is false
+read at
+
 echo $version2
+lt=($(cat .lt))
 
 # start to change version number
 sed -i 's/'$version'/'$version2'/' "templateDetails.xml"
 sed -i '$d' "releases/update.xml"
 sed -i 's/'$version'/'$version2'/' "releases/.newUpdate.xml"
+sed -i 's/raw\/'$version2'/raw\/main/' "releases/newUpdate.xml"
 cat "releases/.newUpdate.xml" >> "releases/update.xml"
 echo "</updates>" >> "releases/update.xml"
 
 zip "releases/msjt_v"$version2".zip" css/* images/* index.php templateDetails.xml
 cp "releases/msjt_v"$version2".zip" "releases/msjt_latest.zip"
 
+if [ $at == "yes" ]
+then
+	git tag -a 'v'$version2 -m 'Add version '$version2''
+	rm .lt
+	touch $version2 .lt
+	sed -i 's/raw\/main/raw\/'$version2'/' "releases/update.xml"
+fi
+
+
 echo Time to update git!
 git add .
 git commit -m 'New version '$version2
 git push origin --all
+git push origin --tags
+
